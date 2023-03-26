a6e893d53b5feb4ecf4230f09d6487b98f454cfc
Adam-Szitas <example.eu.sk.number@gmail.com>
2023-03-26 20:00:51 +0200

    Fetching data from Nest using FTE

--- FTE/src/app/app.component.ts
+++ FTE/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FTE';
+  constructor(private http: HttpClient){}
+
+  title = 'FTE Application';
+  public returnedString: string = "";
+
+  public getHelloWorldFromNest(): void{
+    const url: string = 'http://localhost:3000';
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.append('content-type', 'text/html');
+
+    this.http.get(url, {headers: headers}).pipe(
+      map((data: any) => {
+        this.returnedString = data.text;
+      }),
+      catchError(error => {
+        console.error(error);
+        return of(true);
+      })
+    ).subscribe()
+  }
 }
